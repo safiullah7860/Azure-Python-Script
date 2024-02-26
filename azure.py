e427b48338fe3267f62da624d54a576e28b968cf
safiullah7860 <safiullahbaig7860@gmail.com>
2024-02-25 19:56:08 -0500

    added input + added all-yes to gitignore

--- azure.py
+++ azure.py
@@ -3,8 +3,12 @@ import pandas as pd
 import csv
 import datetime
 import os
-azure_api_key = open("key.txt", "r").read().strip("\n")
-azure_api_endpoint = open("endpoint.txt", "r").read().strip("\n")
+azure_api_key = open("key.txt", "r").read().strip("\n")  #api key
+azure_api_endpoint = open("endpoint.txt", "r").read().strip("\n") #endpoint url
+criteria_text = open("criteria.txt", "r").read().strip("\n") #criteria to feed chatgpt
+# Get the current date and time in a format that is compatible with the CSV file name:
+date_time = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M")
+csv_file_name = f"output_{date_time}.csv"
 client = AzureOpenAI(
     api_version="2023-07-01-preview",
     api_key = azure_api_key,
@@ -12,7 +16,7 @@ client = AzureOpenAI(
 )
 
 completion = client.chat.completions.create(
-    model="FirstModel",  # e.g. gpt-35-instant
+    model="FirstModel", 
     messages=[
         {
             "role": "user",
@@ -21,7 +25,9 @@ completion = client.chat.completions.create(
     ],
 )
 
-# Access the response content directly
 response = completion.choices[0].message.content
 
-print('\n'+ response)
+print('\n'+ response) #gives output in non-JSON format
+os.startfile(csv_file_name)
+
+print("Output saved in", csv_file_name)
