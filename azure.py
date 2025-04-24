a2d2e6596cf781442d7d4d736a65795372e1a34f
safiullah7860 <safiullahbaig7860@gmail.com>
2025-04-23 21:39:24 -0400

    Less Errors for filenames

--- azure.py
+++ azure.py
@@ -8,7 +8,7 @@ import PyPDF2
 
 def pdf_to_text(pdf_path):
     with open(pdf_path, 'rb') as pdf_file:
-        pdf_reader = PyPDF2.PdfReader(pdf_file)
+        pdf_reader = PyPDF2.PdfReader(pdf_file, strict=False)
         abstractText = ''
         for page_num in range(len(pdf_reader.pages)):
             page = pdf_reader.pages[page_num]
